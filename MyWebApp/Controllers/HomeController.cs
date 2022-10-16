1894389cdba13907bc18c57edf840c098040da9c
ArtemBatkov <artembatkov2015@gmail.com>
2022-10-16 19:51:07 +0300

    Step 2: -  Added a new Controller Method to the "HomeController" named "Intro"

--- MyWebApp/Controllers/HomeController.cs
+++ MyWebApp/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace MyWebApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Intro()
+        {
+            return View();
+        }
     }
 }
